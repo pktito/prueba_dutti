c06bf39fb21664a73d2b6f82b2003447dfebe3e4
Roberto <roberto.zazo@gmail.com>
2019-01-27 21:01:29 +0100

    2.1 | 2.2 | Component md-list receive model to show
    - add new input to receive model of list
    - iterate model to show attribute : Name
    - Integrate new flow from model on Ship View

--- src/app-components/md-list/md-list.js
+++ src/app-components/md-list/md-list.js
@@ -5,6 +5,7 @@
         .component('customMdList', {
             templateUrl: 'app-components/md-list/md-list.html',
             bindings: {
+                model: '=model',
                 title: '=title  ',
             },
             controller: function CustomMdListController () {
