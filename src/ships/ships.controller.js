819224f7cf98cc8a41f7aaa92183698d95956baf
Roberto <roberto.zazo@gmail.com>
2019-01-27 20:10:42 +0100

    Create view ships
    add controller dummy for view Ships
    add template dummy for show Title in section

---
+++ src/ships/ships.controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ShipsController', ShipsController);
+
+    ShipsController.$inject = ['$scope'];
+    function ShipsController($scope) {
+        $scope.title = 'Section Ships';
+    }
+
+})();
