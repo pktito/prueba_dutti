36486b9abc32d07ca9ac08163aa10ea9fe5ebd78
Roberto <roberto.zazo@gmail.com>
2019-01-28 17:45:32 +0100

    3.1 | add photo view
    add new view for show design photobook

---
+++ src/photo-style/photo-style.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('PhotoStyleController', PhotoStyleController);
+
+    PhotoStyleController.$inject = ['$scope', '$location', 'model'];
+
+    function PhotoStyleController($scope, $location, model) {
+        $scope.model = model.results;
+        console.log('$scope.photoModel', $scope.model);
+        $scope.title = '#DessedinDutti';
+        $scope.description = {
+            first: "Welcome to MassimoDutti's community.",
+            second: "Shop your favorite looks and share your pictures below to be featured on our site.",
+            third: "Tag us on Instagram:",
+            hashTag: "#DressedinDutti"
+        };
+
+        $scope.goToLocation = function (){
+            $location.path('/ships');
+        };
+    }
+
+})();
