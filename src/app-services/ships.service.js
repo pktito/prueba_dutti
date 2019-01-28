d81eea2521f615a83ed1b3fd1bd3534a657f68a9
Roberto <roberto.zazo@gmail.com>
2019-01-28 20:11:09 +0100

    3.3 | add new http get service for Ship by ID

--- src/app-services/ships.service.js
+++ src/app-services/ships.service.js
@@ -6,16 +6,23 @@
         .factory('ShipsService', ShipsService);
 
     ShipsService.$inject = ['$http'];
+
     function ShipsService($http) {
         let service = {};
 
         service.GetShips = GetShips;
+        service.GetShipsById = GetShipsById;
 
         return service;
 
         function GetShips() {
-            const headers = { 'Authorization': 'none' };
-            return $http.get('https://swapi.co/api/starships/', { headers })
+            const headers = {'Authorization': 'none'};
+            return $http.get('https://swapi.co/api/starships/', {headers})
+        }
+
+        function GetShipsById(id) {
+            const headers = {'Authorization': 'none'};
+            return $http.get('https://swapi.co/api/starships/' + id, {headers})
         }
     }
 })();
