0ee8ebea7a6cd69a93e9e6bc60565b61043739d3
Roberto <roberto.zazo@gmail.com>
2019-01-28 17:45:05 +0100

    3.1 | add UI to ships view
    - add styles baseds on Bootstrap grid
    - add new routes for show photo view

--- src/ships/ships.controller.js
+++ src/ships/ships.controller.js
@@ -5,16 +5,21 @@
         .module('app')
         .controller('ShipsController', ShipsController);
 
-    ShipsController.$inject = ['$scope', 'ShipsService'];
+    ShipsController.$inject = ['$scope', '$location', 'ShipsService'];
 
-    function ShipsController($scope, ShipsService) {
+    function ShipsController($scope, $location, ShipsService) {
         $scope.title = '#DessedinDutti';
-        $scope.description = "Welcome to MassimoDutti's community. Shop your favorite looks and share your pictures below to be featured on our site. Tag us on Instagram: #DressedinDutti";
+        $scope.description = {
+            first: "Welcome to MassimoDutti's community.",
+            second: "Shop your favorite looks and share your pictures below to be featured on our site.",
+            third: "Tag us on Instagram:",
+            hashTag: "#DressedinDutti"
+        };
 
         $scope.shipsValues = [];
         $scope.actualShips = [];
 
-        $scope.pagination = { limit: 5, total: 0, page: 0 };
+        $scope.pagination = {limit: 5, total: 0, page: 0};
         $scope.current = 0;
 
         initController();
@@ -31,7 +36,7 @@
 
             $scope.pagination.page = ($scope.pagination.page + 1) <= ($scope.pagination.total / $scope.pagination.limit)
                 ? $scope.pagination.page + 1
-                : $scope.pagination.page ;
+                : $scope.pagination.page;
 
         }
 
@@ -47,8 +52,12 @@
             console.log('onError', error);
         }
 
-        $scope.getMore = function() {
+        $scope.getMore = function () {
             paginationChange();
+        };
+
+        $scope.goToLocation = function (){
+            $location.path('/photo');
         };
     }
 
